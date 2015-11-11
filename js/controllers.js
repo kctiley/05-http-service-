bb45cd36e9d0cffc5b60e28f5e78ee5bfaf4409b
Klint Tiley <kctiley@gmail.com>
2015-11-10 21:17:13 -0700

    /service path now working

--- js/controllers.js
+++ js/controllers.js
@@ -7,11 +7,11 @@ app.controller('HomeController', function($scope) {
   $scope.homeMessage = "Home!";
 });
 
-app.controller('ServiceController', function($scope) {
+app.controller('ServiceController', function($scope, $http) {
   console.log('test1')
 
   $http.get('https://api.github.com/zen').then(function(data){
     console.log('test2')
-    $scope.zenData = data;
+    $scope.zenData = data.data;
   });
 });
